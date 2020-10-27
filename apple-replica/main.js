cc44d5ed5b0e30f8fb1ad15f060eaf5887e1ff77
Chris <54852438+CD00RT@users.noreply.github.com>
2020-10-27 20:18:59 +0100

    commit

--- apple-replica/main.js
+++ apple-replica/main.js
@@ -1,1 +1,6 @@
-//JavaScript
+const selectElement = (element) => document.querySelector(element)
+
+selectElement('.mobile-menu').addEventListner('click', () => {
+    selectElement('header').classList.toggle('active')
+})
+
